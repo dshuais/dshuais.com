737cfa17f9d60a72edf2153fd57518fb787a9ca6
dshuais <1137896420@qq.com>
2024-02-26 12:20:40 +0800

    ✨ feat: 新增开屏图片加载动效

---
+++ src/components/loadingImg/index.tsx
@@ -0,0 +1,17 @@
+/*
+ * @Author: dushuai
+ * @Date: 2024-02-26 12:19:08
+ * @LastEditors: dushuai
+ * @LastEditTime: 2024-02-26 12:19:21
+ * @description: 开屏图片加载
+ */
+export default defineComponent({
+  name: 'LoadingImg',
+  setup() {
+    return () => (
+      <div>
+
+      </div>
+    )
+  }
+})
