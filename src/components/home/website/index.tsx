b76a61cfa84e6c4334798cc57cf41b175e941a45
dshuais <1137896420@qq.com>
2024-03-11 15:29:16 +0800

    ✨ feat: 新增keepdesign icon

--- src/components/home/website/index.tsx
+++ src/components/home/website/index.tsx
@@ -1,10 +1,10 @@
-import { Card, Icon } from "#components"
+import { Card, Icon, NuxtImg } from "#components"
 
 /*
  * @Author: dushuai
  * @Date: 2024-03-11 14:35:51
  * @LastEditors: dushuai
- * @LastEditTime: 2024-03-11 14:58:41
+ * @LastEditTime: 2024-03-11 15:28:36
  * @description: 网站列表
  */
 export default defineComponent({
@@ -17,10 +17,14 @@ export default defineComponent({
       <div class="flex justify-between flex-wrap">
         {
           websites.value.map(website => (
-            <Card onClick={_ => window.open(website.href, "_blank")} class="mb-2">
+            <Card onClick={_ => window.open(website.href, "_blank")} class="mb-2 group">
               <div class="w-36 h-20 flex justify-center items-center text-xl flex-shrink-0">
-                <Icon name={website.icon} class="text-2xl mr-1" />
-                <div>{website.name}</div>
+                {
+                  website.icon != 'k' ?
+                    <Icon name={website.icon} class="text-2xl group-hover:text-[#00c16a] transition-[color]" /> :
+                    <NuxtImg src={getImageUrl('k.png')} class="w-6 h-6 grayscale group-hover:grayscale-0 transition-[filter]" />
+                }
+                <div class="ml-1">{website.name}</div>
               </div>
             </Card>
           ))
